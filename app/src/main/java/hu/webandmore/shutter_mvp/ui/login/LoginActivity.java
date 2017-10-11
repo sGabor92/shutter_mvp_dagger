6cb504d46701de602efcae0a346175f279c22e43
Chrome <belfegor219@gmail.com>
2017-10-11 15:20:07 +0200

    Created TokenStorage #1

--- app/src/main/java/hu/webandmore/shutter_mvp/ui/login/LoginActivity.java
+++ app/src/main/java/hu/webandmore/shutter_mvp/ui/login/LoginActivity.java
@@ -6,11 +6,27 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
+import butterknife.BindView;
 import hu.webandmore.shutter_mvp.R;
 import hu.webandmore.shutter_mvp.api.model.User;
 
 public class LoginActivity extends AppCompatActivity implements LoginScreen {
+
+    @BindView(R.id.email)
+    private EditText mEmailView;
+
+    @BindView(R.id.password)
+    private EditText mPasswordView;
+
+    @BindView(R.id.login_progress)
+    private ProgressBar mProgressView;
+
+    @BindView(R.id.loginLayout)
+    private LinearLayout mMainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
