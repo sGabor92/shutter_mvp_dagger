bd36312219d40051b600907ce638f8772f962993
Chrome <belfegor219@gmail.com>
2017-10-11 13:08:39 +0200

    Created generic Presenter class for MVP #1 #2

---
+++ app/src/main/java/hu/webandmore/shutter_mvp/ui/Presenter.java
@@ -0,0 +1,15 @@
+package hu.webandmore.shutter_mvp.ui;
+
+public abstract class Presenter<S> {
+
+    protected S screen;
+
+    public void attachScreen(S screen){
+        this.screen = screen;
+    }
+
+    public void detachScreen() {
+        this.screen = null;
+    }
+
+}
